0244ef57bed1dedceceecba75770fdb3f57643a3
louislee-ucla <louiswklee@gmail.com>
2019-10-10 00:46:34 -0700

    Assignment 1+

--- assignment1/scripts/app.js
+++ assignment1/scripts/app.js
@@ -6,13 +6,29 @@
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope){
         $scope.LunchMenu = "";
+        $scope.MsgColor = "";
+        $scope.BorderWidth = 0;
         $scope.checkLunchMenu = function() {
-            var menu = $scope.LunchMenu.split(",");
-            console.log(menu);
-            if (menu.length > 3) {
-                $scope.LunchMessage = "Too much!";
-            } else {
+            var omenu = $scope.LunchMenu.split(",");
+            var menu = [];
+            
+            for (var i=0; i<omenu.length; i++){
+                if (omenu[i].trim() != "") {
+                    menu.push(omenu[i]);
+                }
+            }
+            if ($scope.LunchMenu == "" || menu.length == 0) {
+                $scope.LunchMessage = "Please enter data first";
+                $scope.MsgColor = "red";
+                $scope.BorderWidth = "1px";
+            } else if (menu.length <= 3) {
                 $scope.LunchMessage = "Enjoy!";
+                $scope.MsgColor = "green";
+                $scope.BorderWidth = "1px";
+            } else {
+                $scope.LunchMessage = "Too much!";
+                $scope.MsgColor = "green";
+                $scope.BorderWidth = "1px";
             }
         };
     }
